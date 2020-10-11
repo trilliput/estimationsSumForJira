b6962e67b5ea15759d42a7b6515a6b733921f67e
trilliput <ishmygol@gmail.com>
2020-10-11 21:16:08 +0200

    Install mocha and chai

---
+++ tests/dummy.spec.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+
+describe("Dummy component", function() {
+    describe("Dummy feature", function() {
+        it("is successfull", function() {
+            expect(1+1).to.equal(2)
+        });
+    });
+});
